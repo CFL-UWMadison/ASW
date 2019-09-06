30b72e296ade0aab263b236243f93546d40d2633
Noah Lottig <nrlottig@wisc.edu>
2019-09-05 21:16:52 -0500

    added atm flux to model

--- R Files/carb_metabolism.R
+++ R Files/carb_metabolism.R
@@ -5,7 +5,7 @@ library(LakeMetabolizer)
 library(lubridate)
 
 #### functions
-carb.bookkkeep <- function(datetime = datetime,ph = ph, t = t, lat = lat, salinity = salinity, alk = alk) {
+carb.bookkkeep <- function(datetime = datetime,ph = ph, t = t, lat = lat, salinity = salinity, alk = alk, zmix=zmix) {
     # Simple bookkeep estimation of metabolism from high frequency pH data
     # model does not account for atmospheric exchange
     nobs <- length(ph)
@@ -20,13 +20,27 @@ carb.bookkkeep <- function(datetime = datetime,ph = ph, t = t, lat = lat, salini
     TA <- convert(alk/1000/1000,"conc","molar2molin",S=salinity,t=t) #convert to molin
     ae <- aquaenv(t = t,S = salinity,TA = TA,pH = ph,SumCO2 = NULL,lat = lat)
     dic <- as.numeric(convert(ae$SumCO2,"conc","molin2molar",S=salinity,t=t))*1e6
+    co2 <- as.numeric(convert(ae$CO2,"conc","molin2molar",S=salinity,t=t))*1e6
+    co2_sat <- as.numeric(convert(ae$CO2_sat,"conc","molin2molar",S=salinity,t=t))*1e6
     irr <- as.integer(is.day(datetime, lat = lat))
     dayI <- irr == 1L
     nightI <- irr == 0L
-    delta.dic <- diff(dic) * (-1)
     
-    nep.day <- delta.dic[dayI]
-    nep.night <- delta.dic[nightI]
+    #Gas fluxes
+    wnd <- wind.scale.base(wnd = 4,wnd.z = 2)
+    k600 <- k.cole.base(wnd)
+    k.gas = k600.2.kGAS.base(k600 = k600,temperature = t,gas =  'CO2')
+    #gas flux out is negative
+    #normalized to z.mix, del_concentration/timestep (e.g., mg/L/10min)
+    gas.flux <- (co2_sat - co2) * (k.gas/48) / zmix 
+    
+    #remove the component of delta.do that is due to gas flux
+    delta.dic <- diff(dic)
+    delta.dic.metab <- delta.dic - gas.flux[1:(length(gas.flux)-1)]
+    delta.dic.metab <- delta.dic.metab * (-1)
+    
+    nep.day <- delta.dic.metab[dayI]
+    nep.night <- delta.dic.metab[nightI]
     
     R <- mean(nep.night,na.rm=TRUE) * nobs
     NEP <- mean(delta.dic,na.rm=TRUE) * nobs
@@ -47,7 +61,14 @@ t <- data$do_wtemp
 lat <- 44
 salinity <- 0.3 
 alk <- 3761
+zmix = 7
 
 #run the model
-out <- carb.bookkkeep(datetime = datetime,ph = ph,t = t,lat = lat,salinity = salinity,alk = alk)
+out <- carb.bookkkeep(datetime = datetime,
+                      ph = ph,
+                      t = t,
+                      lat = lat,
+                      salinity = salinity,
+                      alk = alk,
+                      zmix=zmix)
 out
